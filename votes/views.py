53a703ebe13e00ae797f7ac658350b9f3e0d558e
Gustavo Moreira <gustavomn93@gmail.com>
2018-04-19 16:36:24 -0300

    Root with all polls

--- votes/views.py
+++ votes/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from votes.models import Poll
 
 
 def polls_list(request):
-    ctx = {}
+    ctx = dict(
+        polls=Poll.objects.all(),
+    )
     return render(request, 'votes/vote_list.html', ctx)
